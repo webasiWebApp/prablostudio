2b3b82f383e129268c973a0480f2cfb32c6db89a
webasiWebApp <webasiinfo@gmail.com>
2026-02-16 00:08:41 +0530

    feat: Set up initial global styles, configure Tailwind fonts, and create the root layout for the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${outfit.variable} ${dmSans.variable} ${ptSerif.variable} ${poppins.variable} ${bebas.variable} font-dm-sans`}>
+            <body className={`${outfit.variable} ${dmSans.variable} ${ptSerif.variable} ${poppins.variable} ${bebas.variable} font-poppins`}>
                 {/* We move Navbar, Footer, and Logic into this new component */}
                 <LayoutContent>
                     {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,10 +12,11 @@ module.exports = {
                 secondary: "#FFB347",
             },
             fontFamily: {
-                outfit: ["var(--font-outfit)", "sans-serif"],
-                "dm-sans": ["var(--font-dm-sans)", "sans-serif"],
-                "pt-serif": ["var(--font-pt-serif)", "serif"],
+                // outfit: ["var(--font-outfit)", "sans-serif"],
+                // "dm-sans": ["var(--font-dm-sans)", "sans-serif"],
+                // "pt-serif": ["var(--font-pt-serif)", "serif"],
                 poppins: ["var(--font-poppins)", "sans-serif"],
+                outfit: ["var(--font-poppins)", "sans-serif"],
                 bebas: ["var(--font-bebas)", "sans-serif"],
             },
             animation: {
