b83f0950527c3ad9f4ad73f9c4cb5253756ad769
WEBASI <118072521+webasiWebApp@users.noreply.github.com>
2026-06-05 22:57:23 +0530

    Update Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -12,7 +12,7 @@ const teamMembers = [
     { name: "Naveen Buddhika", role: "Head of Brand Identity & Design", image: "/team/Naveen buddika.jpeg" },
     { name: "Ishan Sangeeth", role: "Head of Video Production", image: "/team/Ishan sangeeth.jpeg" },
     { name: "Givindi Gunasekara", role: "Lead Creative Content Creator", image: "/team/Givindi Gunasekara.jpeg" },
-    { name: "Praveen Maleesha", role: "Lead Web Developer", image: "/team/Praveen Maleesha.jpg" },
+    { name: "Praveen Maleesha", role: "Lead Web Developer", image: "/team/Praveen Maleesha.jpeg" },
     { name: "Pathum Akalanka", role: "Senior Visual Designer", image: "/team/Pathum Akalanka.jpeg" },
     { name: "Anuhas Jayasooriya", role: "Brand Designer", image: "/team/Anuhas jayasooriya.jpeg" },
 ];
